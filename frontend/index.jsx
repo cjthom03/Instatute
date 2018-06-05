b1059982f1e0e40d4cd0b3044ce25f855ff591c4
Charlie Thomas <cjthom03@gmail.com>
2018-06-05 13:23:32 -0700

    Complete session api utils

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//TEST START
+  import {signup, login, logout} from './util/session_api_util';
+//TEST END
+
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById("root");
+
+
+  //TEST START
+    window.signup = signup;
+    window.login = login;
+    window.logout = logout;
+  //TEST END
+
   ReactDOM.render(<h1>udemyclone INSTAtute</h1>, rootEl);
 });
