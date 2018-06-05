47282178afc641ced9c43beb25b485a17c6cfd3a
Charlie Thomas <cjthom03@gmail.com>
2018-06-05 14:25:48 -0700

    Complete store config, write thunk middleware, test store and all thunk action creators and reducers

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 //TEST START
-  import {signup, login, logout} from './util/session_api_util';
+  import {signup, login, logout} from './actions/session_actions';
 //TEST END
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById("root");
+  const store = configureStore();
 
 
   //TEST START
     window.signup = signup;
     window.login = login;
     window.logout = logout;
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
   //TEST END
 
   ReactDOM.render(<h1>udemyclone INSTAtute</h1>, rootEl);
