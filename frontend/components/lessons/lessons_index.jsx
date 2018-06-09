397d13f6c16a8a16b73576f2b5edc445dd94f49c
Charlie Thomas <cjthom03@gmail.com>
2018-06-08 22:49:41 -0700

    Initial styling for lesson index

--- frontend/components/lessons/lessons_index.jsx
+++ frontend/components/lessons/lessons_index.jsx
@@ -5,13 +5,16 @@ import LessonsIndexItem from './lessons_index_item';
 const LessonsIndex = props => {
   let lessons = lessonsToArray(props.lessons);
   return(
-    <div className="lessons-index-container">
-        {lessons.map( lesson =>
-          <LessonsIndexItem
-            key={lesson.id}
-            lesson={lesson}
-            />
-        )}
+    <div className="lessons-main">
+      <div className="lessons-index-container">
+          <div className="lessons-index-header">All Lessons</div>
+          {lessons.map( lesson =>
+            <LessonsIndexItem
+              key={lesson.id}
+              lesson={lesson}
+              />
+          )}
+      </div>
     </div>
   );
 };
--- frontend/components/lessons/lessons_index_item.jsx
+++ frontend/components/lessons/lessons_index_item.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 const LessonsIndexItem = ({ lesson }) => {
-  debugger;
   return(
-    <div>
-      {lesson.order_num}
-      {lesson.title}
-      {lesson.duration}
+    <div className="lesson-item-container">
+      <div className="lesson-item-container-left">
+        <i className="far fa-play-circle"></i>
+        {lesson.order_num}. {lesson.title}
+      </div>
+      <div className="lesson-item-container-right">
+        {lesson.duration}
+        <i className="far fa-circle"></i>
+      </div>
     </div>
   );
 };
