16586fef5bb52d908a53135b9575796fe6e2d55d
Charlie Thomas <cjthom03@gmail.com>
2018-06-14 17:16:11 -0700

    Post-production cleanup, removed unnecessary packages from package json (thunk and logger) - rely on self-rolled THUNK

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,5 +1,4 @@
 import { createStore, applyMiddleware } from 'redux';
-// import logger from 'redux-logger';
 import thunk from '../middleware/thunk';
 import rootReducer from '../reducers/root_reducer';
 
